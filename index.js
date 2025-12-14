9c189d639ac737c14377f2774dd2186012bcc1d8
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2025-12-14 07:56:50 -0300

    fix: nova versão about

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var anoInicioTrabalho = 2016;
+const anoInicioTrabalho = 2016;
 
-window.addEventListener('load', function () {
-	document.querySelector('#anos-idade').innerHTML = calculateAge(new Date(1998, 2, 28));
-	document.querySelector('#ano-atual').innerHTML = (new Date).getFullYear();
-	document.querySelector('#anos-trabalhando').innerHTML = (new Date).getFullYear() - anoInicioTrabalho;
+window.addEventListener("load", () => {
+  const anoAtual = new Date().getFullYear();
+
+  document.getElementById("ano-atual").textContent = anoAtual;
+  document.getElementById("anos-trabalhando").textContent =
+    anoAtual - anoInicioTrabalho;
 });
-
-function calculateAge(birthday) { // birthday is a date
-	var ageDifMs = Date.now() - birthday.getTime();
-	var ageDate = new Date(ageDifMs); // miliseconds from epoch
-	return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
